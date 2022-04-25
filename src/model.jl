af7b168813cf51ae4d099d5bef989102e1ab1b14
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-04-25 17:39:40 -0400

    Remove use of NLPModel

--- src/model.jl
+++ src/model.jl
@@ -1,106 +1,66 @@
-using NLPModels, LinearAlgebra
+using LinearAlgebra
 
-mutable struct CCSAModel{T,S} <: AbstractNLPModel{T,S}
-    meta::NLPModelMeta{T,S}
-    counters::Counters
-    xtol_rel::T # x relative tolerence
-    ρ₀::T # penality weight for obj f₀
-    ρᵢ::AbstractVector{T} # [ncon] penality weight for contraints fᵢ
-    σ::AbstractVector{T} # [nvar] radius of trust region
-end
-
-function CCSAModel(
+mutable struct CCSAOpt{T}
     nvar::Int, # number of variables
     ncon::Int, # number of constraints
     x0::AbstractVector{T}, # initial point (must be feasible)
     lvar::AbstractVector{T}, # vector of lower bounds
     uvar::AbstractVector{T}, # vector of upper bounds
     xtol_rel::T, # x relative tolerence
-    ρ₀::T, # penality weight for obj f₀
-    ρᵢ::AbstractVector{T}, # [ncon] penality weight for contraints fᵢ
+    ρ::T, # [ncon + 1] penality weight for obj f₀
     σ::AbstractVector{T} # [nvar] radius of trust region
-) where {T}
-    CCSAModel(
-        NLPModelMeta(nvar;
-            x0=x0,
-            lvar=lvar,
-            uvar=uvar,
-            ncon=ncon),
-        Counters(),
-        xtol_rel,
-        ρ₀,
-        ρᵢ,
-        σ)
+    fᵢJ::Function
 end
 
-function dualModel(
-    nvar::Int, # number of variables
-    x0::AbstractVector{T}, # initial point (must be feasible)
-    xtol_rel::T, # x relative tolerence
-    ρ₀::T, # penality weight for obj f₀
-    σ::AbstractVector{T} # [nvar] radius of trust region
-) where {T}
-    CCSAModel(
-        NLPModelMeta(nvar;
-            x0=x0,
-            lvar=zeros(T, nvar)),
-        Counters(),
-        xtol_rel,
-        ρ₀,
-        T[],
-        σ)
-end
-
-function inner_iter(model::CCSAModel, xᵏ::AbstractVector{T}) where {T}
-    @lencheck model.meta.nvar xᵏ
-    σ² = model.σ .^ 2
-    x_min = max.(xᵏ - model.σ, model.meta.lvar) # trust region lower bound
-    x_max = min.(xᵏ + model.σ, model.meta.uvar) # trust region upper bound
-    if model.meta.ncon == 0 # a problem without contraints (probably dual)
-        f₀xᵏ, ∇f₀xᵏ = NLPModels.objcons(model, xᵏ)
-        x̂ = xᵏ - ∇f₀xᵏ .* σ² / model.ρ₀
+function inner_iter(opt::CCSAOpt, xᵏ::AbstractVector{T}) where {T}
+    σ² = opt.σ .^ 2
+    x_min = max.(xᵏ - opt.σ, opt.lvar) # trust region lower bound
+    x_max = min.(xᵏ + opt.σ, opt.uvar) # trust region upper bound
+    if opt.ncon == 0 # a problem without contraints (probably dual)
+        f₀xᵏ, ∇f₀xᵏ = NLPModels.objcons(opt, xᵏ)
+        x̂ = xᵏ - ∇f₀xᵏ .* σ² / opt.ρ₀
         return @. min(max(x̂, x_min), x_max)
     end
-    f₀xᵏ = NLPModels.obj(model, xᵏ)
-    ∇f₀xᵏ = NLPModels.grad(model, xᵏ)
-    fᵢxᵏ = NLPModels.cons(model, xᵏ)
-    Jfᵢxᵏ = NLPModels.jac(model, xᵏ)
+    f₀xᵏ = NLPModels.obj(opt, xᵏ)
+    ∇f₀xᵏ = NLPModels.grad(opt, xᵏ)
+    fᵢxᵏ = NLPModels.cons(opt, xᵏ)
+    Jfᵢxᵏ = NLPModels.jac(opt, xᵏ)
     while true
         @eval function approx_objcons(x) # gᵢ around xᵏ, i ∈ {0,1,...,m}
             # approximation of obj f₀ and constraints fᵢ around xᵏ
             # 1st order Taylor + weighted quadratic penality for step size
             Δx = x - xᵏ
-            penality = sum(@. (Δx / model.σ)^2) / 2
-            g₀x = f₀xᵏ + dot(∇f₀xᵏ, Δx) + model.ρ₀ * penality
-            gᵢx = fᵢxᵏ + Jfᵢxᵏ * Δx + model.ρᵢ * penality
+            penality = sum(@. (Δx / opt.σ)^2) / 2
+            g₀x = f₀xᵏ + dot(∇f₀xᵏ, Δx) + opt.ρ₀ * penality
+            gᵢx = fᵢxᵏ + Jfᵢxᵏ * Δx + opt.ρᵢ * penality
             g₀x, gᵢx
         end
         @eval function x_dual(λ::AbstractVector)
-            x̂ = xᵏ - (∇f₀xᵏ + Jfᵢxᵏ' * λ) ./ (model.ρ₀ + dot(λ, model.ρᵢ)) .* σ²
+            x̂ = xᵏ - (∇f₀xᵏ + Jfᵢxᵏ' * λ) ./ (opt.ρ₀ + dot(λ, opt.ρᵢ)) .* σ²
             @. min(max(x̂, x_min), x_max) # bounded by trust region
         end
         dual = dualModel(
-            model.meta.ncon, # number of λᵢ == number of primal constraints
-            ones(T, model.meta.ncon), # initial feasible point
-            model.xtol_rel, # relative tolerence
+            opt.meta.ncon, # number of λᵢ == number of primal constraints
+            ones(T, opt.meta.ncon), # initial feasible point
+            opt.xtol_rel, # relative tolerence
             1.0, # ρ₀
-            ones(T, model.meta.ncon)) # σ
+            ones(T, opt.meta.ncon)) # σ
         @eval function NLPModels.objcons(
             dualModel::CCSAModel,
             λ::AbstractVector
         )
             # obj and gradient for dual problem
             @lencheck dualModel.meta.nvar λ
-            increment!(model, :neval_obj)
-            increment!(model, :neval_grad)
+            increment!(opt, :neval_obj)
+            increment!(opt, :neval_grad)
             x = x_dual(λ) # x(λ)
             g₀x, gᵢx = approx_objcons(x)
             g₀x + dot(λ, gᵢx), gᵢx
         end
         λ_optimal = optimize(dual)
         xᵏ⁺¹ = x_dual(λ_optimal)
-        f₀xᵏ⁺¹ = NLPModels.obj(model, xᵏ⁺¹)
-        fᵢxᵏ⁺¹ = NLPModels.cons(model, xᵏ⁺¹)
+        f₀xᵏ⁺¹ = NLPModels.obj(opt, xᵏ⁺¹)
+        fᵢxᵏ⁺¹ = NLPModels.cons(opt, xᵏ⁺¹)
         g₀xᵏ⁺¹, gᵢxᵏ⁺¹ = approx_objcons(xᵏ⁺¹)
         conservative₀ = g₀xᵏ⁺¹ ≥ f₀xᵏ⁺¹
         conservativeᵢ = gᵢxᵏ⁺¹ .≥ fᵢxᵏ⁺¹
@@ -109,27 +69,28 @@ function inner_iter(model::CCSAModel, xᵏ::AbstractVector{T}) where {T}
                 return xᵏ⁺¹
             end
         else
-            model.ρ₀ *= 2
+            opt.ρ₀ *= 2
         end
-        model.ρᵢ[.!conservativeᵢ] *= 2
+        opt.ρᵢ[.!conservativeᵢ] *= 2
     end
 end
 
-function optimize(model::CCSAModel)
-    xᵏ = copy(model.meta.x0)
-    xᵏ⁺¹ = inner_iter(model, xᵏ)
+function optimize(opt::CCSAOpt)
+    xᵏ = copy(opt.x0)
+    xᵏ⁺¹ = inner_iter(opt, xᵏ)
     Δxᵏ = xᵏ⁺¹ - xᵏ
     signᵏ = signbit.(Δxᵏ) # used to check if x moves monotomically or oscillates
-    while any(@. abs(Δxᵏ / xᵏ) > model.xtol_rel)
-        model.ρ₀ /= 2 # geometrically decrease penality weight to
-        model.ρᵢ /= 2 # allow larger subsequent step if possible
+    while any(@. abs(Δxᵏ / xᵏ) > opt.xtol_rel)
+        opt.ρ /= 2 
+        # geometrically decrease penality weight to
+        # allow larger subsequent step if possible
         xᵏ = xᵏ⁺¹
-        xᵏ⁺¹ = inner_iter(model, xᵏ)
+        xᵏ⁺¹ = inner_iter(opt, xᵏ)
         Δxᵏ = xᵏ⁺¹ - xᵏ
         signᵏ⁺¹ = signbit.(Δxᵏ)
         monotonic = signᵏ .== signᵏ⁺¹ # avoid multiplication
-        model.σ[monotonic] *= 2 # double trust region if xⱼ moves monotomically
-        model.σ[.!monotonic] /= 2 # shrink trust region if xⱼ oscillates
+        opt.σ[monotonic] *= 2 # double trust region if xⱼ moves monotomically
+        opt.σ[.!monotonic] /= 2 # shrink trust region if xⱼ oscillates
         signᵏ = signᵏ⁺¹
     end
     xᵏ⁺¹
