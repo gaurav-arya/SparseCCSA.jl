6c57d6e989329722ba53c87d26f3528014a0cef8
Gaurav Arya <gauravarya272@gmail.com>
2023-01-18 00:10:45 +0800

    In progress changes to optimization

--- src/ccsa.jl
+++ src/ccsa.jl
@@ -1,15 +1,17 @@
 """
-An unambiguous specification of the dual problem.
+This structure contains information about the current primal
+iterate, which is sufficient to specify the dual problem.
 """
-struct DualSpecification{T,L}
-    m::Int # number of variables
-    x::Vector{T} # n x 1 primal iterate 
+struct PrimalIterate{T,L}
+    x::Vector{T} # (n x 1) x 1 primal iterate xᵏ
     fx::Vector{T} # (m+1) x 1 values of objective+constraints of primal
     ∇fx::L # (m+1) x n Jacobian linear operator at x
     ρ::Vector{T} # (m+1) x 1 penality weight of primal
     σ::Vector{T} # n x 1 axes lengths of trust region
     lb::Vector{T} # n x 1 lower bounds on primal solution
     ub::Vector{T} # n x 1 upper bounds on primal solution
+    Δx::Vector{T} # (n x 1) x 1 xᵏ⁺¹ - xᵏ
+    Δx_last::Vector{T} # n x 1 xᵏ - xᵏ⁻¹
 end
 
 """
@@ -25,8 +27,8 @@ end
 A callable structure for evaluating the dual function and its gradient.
 """
 struct DualEvaluator{T, L}
-    spec::DualSpecification{T, L}
-    buffers::DualBuffers{T} 
+    iterate::PrimalIterate{T, L}
+    buffer::DualBuffers{T}
 end
 
 """
@@ -36,134 +38,33 @@ Return the dual objective gλ at λ and mutate
 the dual gradient ∇gλ in-place to its new value.
 """
 function (evaluator::DualEvaluator{T})(∇gλ, λ) where {T}
-    @unpack σ, ρ, fx, ∇fx, lb, ub = evaluator.spec 
-    @unpack a, b, δ = evaluator.buffers
+    @unpack σ, ρ, x, fx, ∇fx, lb, ub = evaluator.iterate
+    @unpack a, b, δ = evaluator.buffer
     λ_all = CatView([one(T)], λ)
 
     @. a = dot(λ_all, ρ) / (2 * σ ^ 2)
     mul!(b, ∇fx', λ_all)
     @. δ = clamp(-b / (2 * a), -σ, σ)
-    @. δ = clamp(δ, lb - p.x, ub - x)
+    @. δ = clamp(δ, lb - x, ub - x)
     gλ = dot(λ_all, fx) + sum(@. a * δ^2 + b * δ)
     mul!(∇gλ, ∇fx, δ)
     @. ∇gλ += fx + sum(abs2, δ / σ) / 2 * ρ
     return gλ
 end
 
-
-opt.dual.f_and_∇f = function (λ) # negative Lagrange dual function and gradient
-    gλ, ∇gλ = dual_func!(λ, opt)
-    -gλ, -∇gλ # minus signs used to change max problem to min problem
-end
-
-mutable struct CCSAState{T <: AbstractFloat, F, L}
-    n::Int # number of variables > 0
-    m::Int # number of inequality constraints ≥ 0
+"""
+The static information (i.e. unchanging during optimization) 
+needed to specify the optimization problem to be solved by CCSA.
+"""
+struct CCSASpecification{T,F}
     lb::Vector{T} # n lower bounds
     ub::Vector{T} # n upper bounds
     f_and_∇f::F # f(x) = (m+1, (m+1) x n linear operator)
-    ρ::Vector{T} # m + 1 penality weight
-    σ::Vector{T} # n radius of trust region
-    x::Vector{T} # current best feasible point
     xtol_rel::T # relative tolerence
     xtol_abs::T # absolute tolerence
     ftol_rel::T # relative tolerence
     ftol_abs::T # absolute tolerence
     max_iters::Int # max number of iterations
-    iters::Int # interation count
-
-    fx::Vector{T} # (m+1) x 1 function values at x
-    ∇fx::L # (m+1) x n linear operator of Jacobian at x
-    a::Vector{T} # n
-    b::Vector{T} # n
-    Δx::Vector{T} # n xᵏ⁺¹ - xᵏ
-    Δx_last::Vector{T} # n xᵏ - xᵏ⁻¹
-    gλ::T # Lagrange dual function value
-    ∇gλ::Vector{T} # m Lagrange dual function gradient
-    RET::Symbol
-    fx_last::Vector{T}
-    dual::CCSAState{T} # Lagrange dual problem if the primal problem has constraints
-    
-
-    function CCSAState(n::Int, # number of variables
-                       m::Int, # number of inequality constraints
-                       f_and_∇f::F,
-                       x₀::Vector{T}; # initial feasible point
-                       # parameters below are optional
-                       ρ::Vector{T} = ones(T, m + 1), # (m + 1) penality weight ρ
-                       σ::Vector{T} = ones(T, n), # n radius of trust region σ
-                       lb::Vector{T} = fill(typemin(T), n), # lower bounds, default -Inf
-                       ub::Vector{T} = fill(typemax(T), n), # upper bounds, default Inf
-                       xtol_rel::T = T(1e-5), # relative tolerence
-                       xtol_abs::T = T(1e-5), # relative tolerence
-                       ftol_rel::T = T(1e-5), # relative tolerence
-                       ftol_abs::T = T(1e-5), # relative tolerence
-                       max_iters::Int = typemax(Int64)
-                       ) where {F, T <: AbstractFloat}
-        fx, ∇fx = f_and_∇f(x₀)
-        opt = new{T,F,typeof(∇fx)}(n,
-                     m,
-                     lb,
-                     ub,
-                     f_and_∇f,
-                     ρ,
-                     σ,
-                     x₀,
-                     xtol_rel,
-                     xtol_abs,
-                     ftol_rel,
-                     ftol_abs,
-                     max_iters,
-                     zero(max_iters),
-                     fx,
-                     ∇fx,
-                     Vector{T}(undef, n),
-                     Vector{T}(undef, n),
-                     Vector{T}(undef, n),
-                     Vector{T}(undef, n),
-                     T(0),
-                     Vector{T}(undef, m + 1),
-                     :RUNNING,
-                     Vector{T}(undef, m + 1)
-                     )
-        if opt.m > 0
-            opt.dual = CCSAState(opt.m, # number of Lagrange multipliers
-                                 0, # no inequality constraints
-                                 λ -> (T[], T[]),
-                                 ones(T, opt.m), # initial feasible point for Lagrange multipliers
-                                 lb = zeros(T, opt.m))
-        end
-        return opt
-    end
-end
-
-# optimize problem with no constraint
-function optimize_simple(opt::CCSAState{T}) where {T}
-    monotonic = BitVector(undef, opt.n)
-    while opt.iters < opt.max_iters
-        opt.fx, opt.∇fx = opt.f_and_∇f(opt.x)
-        opt.a .= opt.ρ[1] / 2 ./ (opt.σ) .^ 2
-        opt.b .= opt.∇fx[:]
-        for i in 1:10
-            @. opt.Δx = clamp(-opt.b / (2 * opt.a), -opt.σ, opt.σ)
-            @. opt.Δx = clamp(opt.Δx, opt.lb - opt.x, opt.ub - opt.x)
-            opt.gλ = opt.fx[1] + sum(@. opt.a * opt.Δx^2 + opt.b * opt.Δx)
-            if opt.gλ ≥ opt.f_and_∇f(opt.x + opt.Δx)[1][1] # check conservative
-                break
-            end
-            opt.ρ *= 2
-            opt.a *= 2
-        end
-        opt.ρ /= 2
-        monotonic .= signbit.(opt.Δx_last) .== signbit.(opt.Δx) # signbit avoid multiplication
-        opt.σ[monotonic] *= 2 # double trust region if xⱼ moves monotomically
-        opt.σ[.!monotonic] /= 2 # shrink trust region if xⱼ oscillates
-        opt.x .+= opt.Δx
-        opt.Δx_last .= opt.Δx
-        if norm(opt.Δx, Inf) < opt.xtol_rel
-            return
-        end
-    end
 end
 
 """
@@ -206,8 +107,68 @@ function inner_iterate(opt::CCSAState{T}) where {T}
     opt.dual.x .= one(T) # reinitialize starting point of Lagrange multipliers
 end
 
-function step!(opt::CCSAState)
+function step!(state::CCSAState, spec::CCSASpecification)
+    @unpack f_and_∇f = spec 
+    @unpack iterate, extra_info = state
+
+
+    # TODO: why 10 inner iterations here?
+    for i in 1:10
+        inner_iterate(opt)
+    end
+    f = opt.fx[1] # Get current value of objective function
+    Δf = opt.f_and_∇f(opt.x+opt.Δx)[1][1] - f # Get change in opt val..? 
+                                                # TODO: is x + Δx what we want/
+    
+    monotonic .= (signbit.(opt.Δx_last).==signbit.(opt.Δx))
+    opt.σ[monotonic] *= 2 # double trust region if xⱼ moves monotomically
+    opt.σ[.!monotonic] /= 2 # shrink trust region if xⱼ oscillates
+    opt.ρ /= 2
+    opt.x .+= opt.Δx
+    opt.Δx_last .= opt.Δx
+    opt.iters += 1
 end
+
+struct Solution{T}
+    x::Vector{T}
+    RET::Symbol
+end
+
+function optimize!(state::CCSAState, spec::CCSASpecification{T})
+end
+
+function optimize(spec::CCSASpecification{T}, x0::Vector{T}, lb::Vector{T}, ub::Vector{T}) where {T}
+    fx, ∇fx = spec.f_and_∇f(x0)
+    if any(@view f[2:end] .> 0)
+        return ODESolution(x0, :INFEASIBLE)
+    end
+    n = length(x)
+    dual_spec = DualSpecification(x0, fx, ∇fx, ones(T, m+1), ones(T, n), ones(T, n), ones(T, n))
+    extra_info = ExtraInfo(zeros(T, n), zeros(T, n))
+    state = CCSAState(dual_spec, extra_info)
+    for iter in 1:spec.max_iters 
+        step!(state, spec)
+        # Check termination conditions
+        if norm(opt.Δx, Inf) < opt.xtol_abs
+            opt.RET = :XTOL_ABS
+            return
+        end
+        if norm(opt.Δx, Inf) / norm(opt.x, Inf)  < opt.xtol_rel
+            opt.RET = :XTOL_REL
+            return
+        end
+        if norm(Δf, Inf) < opt.ftol_abs
+            opt.RET = :FTOL_REL
+            return
+        end
+        if norm(Δf, Inf) / norm(f, Inf) < opt.ftol_rel
+            opt.RET = :FTOL_REL
+            return
+        end
+    end
+    return Solution(state.dual_spec.x, :MAX_ITERS)
+end
+
 
 """
     optimize(opt::CCSAState; callback = nothing)
@@ -283,3 +244,34 @@ function optimize(opt::CCSAState; callback = nothing)
     end
     return opt.iters
 end
+
+
+# optimize problem with no constraint
+function optimize_simple(opt::CCSAState{T}) where {T}
+    monotonic = BitVector(undef, opt.n)
+    while opt.iters < opt.max_iters
+        opt.fx, opt.∇fx = opt.f_and_∇f(opt.x)
+        opt.a .= opt.ρ[1] / 2 ./ (opt.σ) .^ 2
+        opt.b .= opt.∇fx[:]
+        for i in 1:10
+            @. opt.Δx = clamp(-opt.b / (2 * opt.a), -opt.σ, opt.σ)
+            @. opt.Δx = clamp(opt.Δx, opt.lb - opt.x, opt.ub - opt.x)
+            opt.gλ = opt.fx[1] + sum(@. opt.a * opt.Δx^2 + opt.b * opt.Δx)
+            if opt.gλ ≥ opt.f_and_∇f(opt.x + opt.Δx)[1][1] # check conservative
+                break
+            end
+            opt.ρ *= 2
+            opt.a *= 2
+        end
+        opt.ρ /= 2
+        monotonic .= signbit.(opt.Δx_last) .== signbit.(opt.Δx) # signbit avoid multiplication
+        opt.σ[monotonic] *= 2 # double trust region if xⱼ moves monotomically
+        opt.σ[.!monotonic] /= 2 # shrink trust region if xⱼ oscillates
+        opt.x .+= opt.Δx
+        opt.Δx_last .= opt.Δx
+        if norm(opt.Δx, Inf) < opt.xtol_rel
+            return
+        end
+    end
+end
+
