f11e4f57f7c02e3785f7a06b9322581e6dbe335b
Gaurav Arya <gauravarya272@gmail.com>
2023-04-08 18:59:11 -0400

    Add nlopt implementation of rosebrock disk and start comparing

--- examples/rosenbrock_disk.jl
+++ examples/rosenbrock_disk.jl
@@ -37,8 +37,70 @@ begin
     dual_iterate = dual_optimizer.iterate
 end
 
-for i in 1:300
+for i in 1:1
     step!(opt)
 end
 
 opt.iterate |> dump
+
+## Try NLOpt
+
+using NLopt
+
+function obj(x, grad)
+    if length(grad) > 0
+        grad .= ForwardDiff.gradient(x -> f(x)[1], x)
+    end
+    return f(x)[1]
+end
+
+function cons1(x, grad)
+    if length(grad) > 0
+        grad .= ForwardDiff.gradient(x -> f(x)[2], x)
+    end
+    return f(x)[2]
+end
+
+function run_once_nlopt()
+    nlopt = Opt(:LD_MMA, 2)
+    nlopt.lower_bounds = [-1.0, -1.0]
+    nlopt.maxeval = 6
+    # nlopt.xtol_rel = 1e-4
+    nlopt.params["verbosity"] = 2
+
+    nlopt.min_objective = obj 
+    inequality_constraint!(nlopt, cons1)
+
+    (minf,minx,ret) = optimize(nlopt, [0.5, 0.5])
+    return minf,minx,ret
+end
+
+function run_once_mine()
+    opt = init(f_and_jac, fill(-1.0, 2), fill(1.0, 2), n, m;
+               x0 = [0.5, 0.5], max_iters = 5,
+               max_inner_iters = 20,
+               max_dual_iters = 50, max_dual_inner_iters = 50,
+               jac_prototype = zeros(m + 1, n))
+    dual_optimizer = opt.dual_optimizer
+    dual_iterate = dual_optimizer.iterate
+
+    step!(opt)
+    return opt
+end
+
+run_once_nlopt()
+
+run_once_mine();
+
+##
+
+nlopt.numevals
+
+opt.iterate.fx[1]
+
+f([0.5, 0.5])[1]
+f([0.603, 0.397])[1]
+f(opt.iterate.x)[1]
+
+opt.iterate.x
+opt.iterate.ρ
