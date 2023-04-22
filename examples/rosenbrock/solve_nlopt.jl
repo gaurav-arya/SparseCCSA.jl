5ca6b2ec0cea6a344f1cebd21c1ebeb02f330865
Gaurav Arya <gauravarya272@gmail.com>
2023-04-22 19:24:59 -0400

    Better nlopt output parsing

--- examples/rosenbrock/solve_nlopt.jl
+++ examples/rosenbrock/solve_nlopt.jl
@@ -26,16 +26,10 @@ function run_once_nlopt(iters=1, evals=nothing)
 
     nlopt.min_objective = obj 
     inequality_constraint!(nlopt, cons1)
-    return nlopt
 
-    # (minf,minx,ret) = optimize(nlopt, [0.5, 0.5])
-    # return minf,minx,ret
+    (minf,minx,ret) = optimize(nlopt, [0.5, 0.5])
+    return minf,minx,ret
 end
-nlopt = run_once_nlopt(nothing, 7)
-
-nlopt.params["verbosity"] = 2
-optimize(nlopt, [0.5,0.5])
-Base.flush_cstdio()
 
 ## NLOpt output processing
 
@@ -52,68 +46,58 @@ function safe_scanf(buffer, fmt, args...)
     return out
 end
 
-open("out1.txt", "w") do io
+open("nlopt_out.txt", "w") do io
     redirect_stdout(io) do
         run_once_nlopt(nothing, 20)
         Base.Libc.flush_cstdio()
     end
 end
-out_str = read(open("out1.txt"), String)
+out_str = read(open("nlopt_out.txt"), String)
 lines = split(out_str, "\n")
 
 nlopt_res = DataFrame()
 
 buffer = IOBuffer(out_str)
-line_1 = Scanf.format"CCSA dual converged in 3 iters to g=%f:"
-scanf(buffer, line_1, Float64)
 
-inner_iter = Scanf.format"""
+inner_iter_fmt = Scanf.format"""
 CCSA dual converged in %d iters to g=%f:
     CCSA y[0]=%f, gc[0]=%f
 """
-inner_iter2 = Scanf.format"""
+inner_iter2_fmt = Scanf.format"""
 CCSA inner iteration: rho -> %f 
                 CCSA rhoc[0] -> %f
 """
-outer_iter = Scanf.format"""
+outer_iter_fmt = Scanf.format"""
 CCSA outer iteration: rho -> %f
                  CCSA rhoc[0] -> %f
+"""
+outer_iter_sigma_fmt = Scanf.format"""
                  CCSA sigma[0] -> %f
 """
 buffer = IOBuffer(out_str)
-scanf(buffer, inner_iter, Int64, (Float64 for i in 1:3)...) 
-pos = position(buffer)
-scanf(buffer, inner_iter2, (Float64 for i in 1:2)...) 
-seek(buffer, pos)
-
-
-pos = position(buffer)
-scanf(buffer, outer_iter, (Float64 for i in 1:3)...) 
-seek(buffer, pos)
-
-buffer |> dump
+safe_scanf(buffer, inner_iter, Int64, (Float64 for i in 1:3)...) 
+safe_scanf(buffer, inner_iter2, (Float64 for i in 1:2)...) 
+safe_scanf(buffer, outer_iter, (Float64 for i in 1:2)...) 
+safe_scanf(buffer, outer_iter_sigma, Float64) 
 
 open("out.txt", "w") do f
     write(f, read(copy(buffer), String)) 
 end
 
-
-line_1 = "CCSA dual converged in 3 iters to g=%f:"
-r, a, b = @scanf "23.4 text" "%f %2s" Float64 String
+buffer = IOBuffer(out_str)
 while true 
     # read one inner iteration
     d = DataFrame()
     inner_iter = 0
     while true
-        line_1 = """
-CCSA dual converged in 3 iters to g=%f:
-    CCSA y[0]=0, gc[0]=%f
-CCSA inner iteration: rho -> %f 
-                CCSA rhoc[0] -> %f
-        """
-        @show line
-        @scanf line line_1
-        break
+        dual_iters, dual_obj = safe_scanf(buffer, inner_iter_fmt, Int64, (Float64 for i in 1:3)...) 
+        if (out = safe_scanf(buffer, inner_iter2_fmt, (Float64 for i in 1:2)...)) !== nothing
+            rho, rhoc = out
+        else
+            rho, rhoc = NaN, NaN
+            break
+        end
+        push!(d, (;dual_iters, dual_obj, rho, rhoc))
     end
     break
     # push!(nlopt_res, Dict(:iter => iter, :fx => NaN, :x => NaN, :ρ => NaN, :σ => NaN))
