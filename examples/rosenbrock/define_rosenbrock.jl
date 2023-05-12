0c799e0b2212165c2905ec033a15e5a286dcb689
Gaurav Arya <gauravarya272@gmail.com>
2023-05-12 18:03:19 -0400

    Revert "tweaks"
    This reverts commit c6796d1013cf8d110e17672a2429dab52b7eaae4.

--- examples/rosenbrock/define_rosenbrock.jl
+++ examples/rosenbrock/define_rosenbrock.jl
@@ -4,10 +4,10 @@ using ForwardDiff
 using SparseCCSA
 
 function f(x)
-    # obj = x[1]^4#
-    obj = 100 * (x[2] - x[1]^2)^2 + (1 - x[1])^2
-    # cons = x[1]^2 - 1
-    cons = x[1]^2 + x[2]^2 - 1
+    obj = x[1]^4#
+    # obj = 100 * (x[2] - x[1]^2)^2 + (1 - x[1])^2
+    cons = x[1]^2 - 1
+    # cons = x[1]^2 + x[2]^2 - 1
     return [obj, cons]
 end
 
