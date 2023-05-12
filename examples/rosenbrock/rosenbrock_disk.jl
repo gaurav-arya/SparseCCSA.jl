0516a6d344c8eabfb25fd61f0ab92775b8c62cdd
Gaurav Arya <gauravarya272@gmail.com>
2023-05-12 18:29:39 -0400

    Progress making plots comparing to nlopt

--- examples/rosenbrock/rosenbrock_disk.jl
+++ examples/rosenbrock/rosenbrock_disk.jl
@@ -3,36 +3,38 @@ include("define_rosenbrock.jl")
 include("solve_nlopt.jl")
 include("solve_sparseccsa.jl")
 
-run_once_nlopt(8)
-df_sp = sparseccsa_df(2)
+#run_once_nlopt(8)
+df_sp = sparseccsa_df(100)
+df_n = nlopt_df(230)
 
-df_sp.inner_history[2]
-
-df_n = nlopt_df(10)
-df_sp = sparseccsa_df(2)
-
-df_n
-df_sp[1, :]
-sparse_n = df_sp.inner_history[2]
-inner_n = df_n.inner_history[2]
-df_n.inner_history[2].x_proposed[end] - df_n.inner_history[1].x_proposed[end]
-
-fx = zeros(2)
-jac_fx = zeros(2, 2)
-f_and_jac(fx, jac_fx, df_sp.x[1])
-jac_fx
-
-inner3_n.ρ .- sparse3_n.ρ
-
-
-
-
+sparse_n = df_sp.inner_history[7]
+inner_n = df_n.inner_history[7]
+isapprox(df_n.inner_history[9].x_proposed[end], df_sp.inner_history[9].x_proposed[end]; rtol=1e-3)
 
 # some plotting
 
-using GLMakie
-lines(first.(df_n.σ))
-lines(first.(df_sp.σ))
+using CairoMakie
+CairoMakie.activate!()
+
+begin
+fig = Figure()
+# σ
+ax = Axis(fig[1, 1]; xlabel="Iterations", yscale=log10)
+lines!(ax, first.(df_sp.σ); label="SparseCCSA σ", linestyle=:dash)
+lines!(ax, first.(df_n.σ); label="nlopt σ", linestyle=:dot)
+axislegend(ax; position=:rt)
+# ρ
+ax = Axis(fig[1, 2]; xlabel="Iterations", yscale=log10)
+lines!(ax, first.(df_sp.ρ); label="SparseCCSA ρ", linestyle=:dash)
+lines!(ax, first.(df_n.ρ); label="nlopt ρ", linestyle=:dot)
+axislegend(ax; position=:rt)
+#
+fig
+end
+# makie add legend to ax
+
+fig
+
 lines(log.(first.(df_n.ρ)))
 lines(log.(first.(df_sp.ρ)))
 lines(log.(last.(df_sp.ρ)))
