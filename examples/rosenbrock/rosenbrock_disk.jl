25cb1789eadc93ffb3147981812c363a09ac3f23
Gaurav Arya <gauravarya272@gmail.com>
2023-05-12 23:42:31 -0400

    More plot improvements

--- examples/rosenbrock/rosenbrock_disk.jl
+++ examples/rosenbrock/rosenbrock_disk.jl
@@ -2,14 +2,18 @@
 include("define_rosenbrock.jl")
 include("solve_nlopt.jl")
 include("solve_sparseccsa.jl")
+using LinearAlgebra
 
 #run_once_nlopt(8)
-df_sp = sparseccsa_df(100)
-df_n = nlopt_df(230)
+df_sp = sparseccsa_df(30)
+df_n = nlopt_df(110)
+df_n_long = nlopt_df(1000)
 
 sparse_n = df_sp.inner_history[7]
 inner_n = df_n.inner_history[7]
-isapprox(df_n.inner_history[9].x_proposed[end], df_sp.inner_history[9].x_proposed[end]; rtol=1e-3)
+isapprox(df_n.x[9], df_sp.x[9]; rtol=1e-3)
+
+df_sp.x[30]
 
 # some plotting
 
@@ -17,25 +21,34 @@ using CairoMakie
 CairoMakie.activate!()
 
 begin
-fig = Figure()
+fig = Figure(resolution = (1200, 600))
 # σ
 ax = Axis(fig[1, 1]; xlabel="Iterations", yscale=log10)
-lines!(ax, first.(df_sp.σ); label="SparseCCSA σ", linestyle=:dash)
-lines!(ax, first.(df_n.σ); label="nlopt σ", linestyle=:dot)
-axislegend(ax; position=:rt)
+lines!(ax, norm.(df_sp.σ); label="SparseCCSA σ", linestyle=:dash)
+lines!(ax, norm.(df_n.σ); label="nlopt σ", linestyle=:dot)
+# axislegend(ax; position=:rt)
 # ρ
-ax = Axis(fig[1, 2]; xlabel="Iterations", yscale=log10)
+# ax = Axis(fig[1, 2]; xlabel="Iterations", yscale=log10)
 lines!(ax, first.(df_sp.ρ); label="SparseCCSA ρ", linestyle=:dash)
 lines!(ax, first.(df_n.ρ); label="nlopt ρ", linestyle=:dot)
-axislegend(ax; position=:rt)
+axislegend(ax; position=:lt)
 #
+ax = Axis(fig[1, 2]; xlabel="Iterations")
+lines!(ax, first.(df_sp.x); label="SparseCCSA x[1]", linestyle=:dash)
+lines!(ax, first.(df_n.x); label="nlopt x[1]", linestyle=:dot)
+lines!(ax, last.(df_sp.x); label="SparseCCSA x[2]", linestyle=:dash)
+lines!(ax, last.(df_n.x); label="nlopt x[2]", linestyle=:dot)
+axislegend(ax; position=:rb)
+# lines!(ax, first.(df_n.x); label="nlopt ρ", linestyle=:dot)
+# axislegend(ax; position=:rt)
+#
+ax = Axis(fig[1, 3]; xlabel="Iterations", yscale=log10)
+finalx = df_n_long.x[end]
+lines!(ax, abs.(first.(df_sp.x .- Ref(finalx))); label="SparseCCSA x[1]", linestyle=:dash)
+lines!(ax, abs.(first.(df_n.x .- Ref(finalx))); label="nlopt x[1]", linestyle=:dot)
+lines!(ax, abs.(last.(df_sp.x .- Ref(finalx))); label="SparseCCSA x[2]", linestyle=:dash)
+lines!(ax, abs.(last.(df_n.x .- Ref(finalx))); label="nlopt x[2]", linestyle=:dot)
+axislegend(ax; position=:lb)
 fig
+
 end
-# makie add legend to ax
-
-fig
-
-lines(log.(first.(df_n.ρ)))
-lines(log.(first.(df_sp.ρ)))
-lines(log.(last.(df_sp.ρ)))
-
